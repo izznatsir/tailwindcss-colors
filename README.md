60ed280fc564872e4bac37079f5d763baf3863a6
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-01-13 14:57:19 +0700

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwind CSS Color Variables
+
+CSS color variables that are compatible with Tailwind CSS color opacity syntax.
---
+++ scripts/generate.ts
@@ -0,0 +1,9 @@
+import { ColorTranslator } from "colortranslator";
+import * as tailwindColors from "tailwindcss/colors";
+
+/**
+ * Generate Tailwind CSS compatible CSS color variables.
+ */
+function main() {}
+
+main();

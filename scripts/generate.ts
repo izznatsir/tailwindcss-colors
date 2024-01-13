4c0f8cbc7fc9f759b9f80281069ca7132604c120
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-01-13 16:49:56 +0700

    feat: generate radix colors and split css files

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,3 +1,4 @@
+import radixColors from "@radix-ui/colors";
 import { ColorTranslator } from "colortranslator";
 import tailwindColors from "tailwindcss/colors.js";
 import * as fs from "node:fs";
@@ -7,11 +8,15 @@ import * as path from "node:path";
  * Generate Tailwind CSS compatible CSS color variables.
  */
 function main() {
+	generateRadixColorVariables();
 	generateTailwindColorVariables();
 }
 
 main();
 
+/**
+ * Generate Tailwind color variables.
+ */
 function generateTailwindColorVariables() {
 	const {
 		black,
@@ -29,14 +34,14 @@ function generateTailwindColorVariables() {
 	const singleScaleColors = { black, white };
 
 	let ts = ``;
-	let css = ``;
-
-	// Open :root scope.
-	css += `:root {\n`;
 
 	// Append single scale color variables.
 	for (const color in singleScaleColors) {
+		let css = ``;
+
 		ts += `/** ${color} */\n`;
+
+		css += `:root {\n`;
 		css += `\t/* ${color} */\n`;
 
 		const colorValue =
@@ -45,42 +50,126 @@ function generateTailwindColorVariables() {
 		const variableValue = `${translator.H}deg ${translator.S}% ${translator.L}%`;
 
 		ts += `export const ${color} = "hsl(${variableValue} / <alpha-value>)" as const;\n`;
+
 		css += `\t--color-${color}: ${variableValue};\n`;
+		css += `}\n`;
+
+		writeFileSync({
+			content: css,
+			relativePath: `./dist/css/tailwind/${color}.css`,
+		});
 	}
 
 	// Append color variables.
 	for (const color in colors) {
 		const colorValue = colors[color as keyof typeof colors];
 
+		let css = ``;
+
 		ts += `/** ${color} */\n`;
 		ts += `export const ${color} = {\n`;
+
+		css += `:root {\n`;
 		css += `\t/* ${color} */\n`;
 
 		for (const scale in colorValue) {
 			const scaleValue = colorValue[scale as keyof typeof colorValue];
 			const translator = new ColorTranslator(scaleValue);
+			const variableName = `--color-${color}-${scale}`;
 			const variableValue = `${translator.H}deg ${translator.S}% ${translator.L}%`;
 
-			ts += `\t${scale}: "hsl(${variableValue} / <alpha-value>)",\n`;
-			css += `\t--color-${color}-${scale}: ${variableValue};\n`;
+			ts += `\t${scale}: "hsl(var(${variableName}) / <alpha-value>)",\n`;
+			css += `\t${variableName}: ${variableValue};\n`;
 		}
 
 		ts += `} as const;\n`;
+		css += `}\n`;
+
+		writeFileSync({
+			content: css,
+			relativePath: `./dist/css/tailwind/${color}.css`,
+		});
 	}
 
-	// Close :root scope.
-	css += `}\n`;
-
 	writeFileSync({ content: ts, relativePath: "./src/tailwind.ts" });
-	writeFileSync({ content: css, relativePath: "./dist/css/tailwind.css" });
 }
 
+/**
+ * Generate Radix color variables.
+ */
+function generateRadixColorVariables() {
+	const colors = radixColors;
+
+	let ts = ``;
+
+	// Append color variables.
+	for (const colorWithDark in colors) {
+		if (/P3A?$/.test(colorWithDark)) continue;
+
+		const color = colorWithDark.replace("Dark", "");
+
+		const isDark = /DarkA?$/.test(colorWithDark);
+		const isAlpha = /A$/.test(color);
+
+		const colorValue = colors[colorWithDark as keyof typeof colors];
+
+		let css = ``;
+
+		if (!isDark) {
+			ts += `/** ${color} */\n`;
+			ts += `export const ${color} = {\n`;
+		}
+
+		if (!isDark) {
+			css += `:root {\n`;
+		} else {
+			css += `:root.dark {\n`;
+		}
+		css += `\t/* ${colorWithDark} */\n`;
+
+		for (let colorWithScale in colorValue) {
+			const scale = colorWithScale.replace(color, "");
+
+			const scaleValue = colorValue[colorWithScale as keyof typeof colorValue];
+			const translator = new ColorTranslator(scaleValue);
+			const variableName = `--color-${color}-${scale}`;
+			let variableValue = `${translator.H}deg ${translator.S}% ${translator.L}%`;
+
+			if (isAlpha) variableValue += ` / ${translator.A}`;
+
+			if (!isDark) {
+				ts += `\t${scale}: "hsl(var(${variableName})`;
+				ts += isAlpha ? `)",\n` : ` / <alpha-value>)",\n`;
+			}
+			css += `\t${variableName}: ${variableValue};\n`;
+		}
+
+		if (!isDark) {
+			ts += `} as const;\n`;
+		}
+		css += `}\n`;
+
+		writeFileSync({
+			content: css,
+			relativePath: `./dist/css/radix/${color}.css`,
+			mode: !isDark ? "write" : "append",
+		});
+	}
+
+	writeFileSync({ content: ts, relativePath: "./src/radix.ts" });
+}
+
+/**
+ * Safely write file synchronously.
+ */
 function writeFileSync({
 	content,
 	relativePath,
+	mode,
 }: {
 	content: string;
 	relativePath: string;
+	mode?: "append" | "write";
 }) {
 	const absolutePath = path.resolve(process.cwd(), relativePath);
 	const dirname = path.dirname(absolutePath);
@@ -88,5 +177,9 @@ function writeFileSync({
 	const isDirectoryExist = fs.existsSync(dirname);
 	if (!isDirectoryExist) fs.mkdirSync(dirname, { recursive: true });
 
-	fs.writeFileSync(absolutePath, content, { encoding: "utf-8" });
+	if (mode === "append") {
+		fs.appendFileSync(absolutePath, content, { encoding: "utf-8" });
+	} else {
+		fs.writeFileSync(absolutePath, content, { encoding: "utf-8" });
+	}
 }

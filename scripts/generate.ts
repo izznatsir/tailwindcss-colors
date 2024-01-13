d31900889f1aa80f48bf80aed3c33b9a5226611e
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-01-13 16:03:30 +0700

    feat: generate tailwindcss color variables

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,9 +1,92 @@
 import { ColorTranslator } from "colortranslator";
-import * as tailwindColors from "tailwindcss/colors";
+import tailwindColors from "tailwindcss/colors.js";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 /**
  * Generate Tailwind CSS compatible CSS color variables.
  */
-function main() {}
+function main() {
+	generateTailwindColorVariables();
+}
 
 main();
+
+function generateTailwindColorVariables() {
+	const {
+		black,
+		white,
+		blueGray: _,
+		coolGray: __,
+		current: ___,
+		inherit: ____,
+		lightBlue: _____,
+		transparent: ______,
+		trueGray: _______,
+		warmGray: ________,
+		...colors
+	} = tailwindColors;
+	const singleScaleColors = { black, white };
+
+	let ts = ``;
+	let css = ``;
+
+	// Open :root scope.
+	css += `:root {\n`;
+
+	// Append single scale color variables.
+	for (const color in singleScaleColors) {
+		ts += `/** ${color} */\n`;
+		css += `\t/* ${color} */\n`;
+
+		const colorValue =
+			singleScaleColors[color as keyof typeof singleScaleColors];
+		const translator = new ColorTranslator(colorValue);
+		const variableValue = `${translator.H}deg ${translator.S}% ${translator.L}%`;
+
+		ts += `export const ${color} = "hsl(${variableValue} / <alpha-value>)" as const;\n`;
+		css += `\t--color-${color}: ${variableValue};\n`;
+	}
+
+	// Append color variables.
+	for (const color in colors) {
+		const colorValue = colors[color as keyof typeof colors];
+
+		ts += `/** ${color} */\n`;
+		ts += `export const ${color} = {\n`;
+		css += `\t/* ${color} */\n`;
+
+		for (const scale in colorValue) {
+			const scaleValue = colorValue[scale as keyof typeof colorValue];
+			const translator = new ColorTranslator(scaleValue);
+			const variableValue = `${translator.H}deg ${translator.S}% ${translator.L}%`;
+
+			ts += `\t${scale}: "hsl(${variableValue} / <alpha-value>)",\n`;
+			css += `\t--color-${color}-${scale}: ${variableValue};\n`;
+		}
+
+		ts += `} as const;\n`;
+	}
+
+	// Close :root scope.
+	css += `}\n`;
+
+	writeFileSync({ content: ts, relativePath: "./src/tailwind.ts" });
+	writeFileSync({ content: css, relativePath: "./dist/css/tailwind.css" });
+}
+
+function writeFileSync({
+	content,
+	relativePath,
+}: {
+	content: string;
+	relativePath: string;
+}) {
+	const absolutePath = path.resolve(process.cwd(), relativePath);
+	const dirname = path.dirname(absolutePath);
+
+	const isDirectoryExist = fs.existsSync(dirname);
+	if (!isDirectoryExist) fs.mkdirSync(dirname, { recursive: true });
+
+	fs.writeFileSync(absolutePath, content, { encoding: "utf-8" });
+}
---
+++ src/tailwind.ts
@@ -0,0 +1,312 @@
+/** black */
+export const black = "hsl(0deg 0% 0% / <alpha-value>)" as const;
+/** white */
+export const white = "hsl(0deg 0% 100% / <alpha-value>)" as const;
+/** slate */
+export const slate = {
+	50: "hsl(210deg 40% 98.039216% / <alpha-value>)",
+	100: "hsl(210deg 40% 96.078431% / <alpha-value>)",
+	200: "hsl(214.285714deg 31.818182% 91.372549% / <alpha-value>)",
+	300: "hsl(212.727273deg 26.829268% 83.921569% / <alpha-value>)",
+	400: "hsl(215deg 20.224719% 65.098039% / <alpha-value>)",
+	500: "hsl(215.384615deg 16.317992% 46.862745% / <alpha-value>)",
+	600: "hsl(215.294118deg 19.318182% 34.509804% / <alpha-value>)",
+	700: "hsl(215.294118deg 25% 26.666667% / <alpha-value>)",
+	800: "hsl(217.241379deg 32.58427% 17.45098% / <alpha-value>)",
+	900: "hsl(222.222222deg 47.368421% 11.176471% / <alpha-value>)",
+	950: "hsl(228.571429deg 84% 4.901961% / <alpha-value>)",
+} as const;
+/** gray */
+export const gray = {
+	50: "hsl(210deg 20% 98.039216% / <alpha-value>)",
+	100: "hsl(220deg 14.285714% 95.882353% / <alpha-value>)",
+	200: "hsl(220deg 13.043478% 90.980392% / <alpha-value>)",
+	300: "hsl(216deg 12.195122% 83.921569% / <alpha-value>)",
+	400: "hsl(217.894737deg 10.614525% 64.901961% / <alpha-value>)",
+	500: "hsl(220deg 8.93617% 46.078431% / <alpha-value>)",
+	600: "hsl(215deg 13.793103% 34.117647% / <alpha-value>)",
+	700: "hsl(216.923077deg 19.117647% 26.666667% / <alpha-value>)",
+	800: "hsl(215deg 27.906977% 16.862745% / <alpha-value>)",
+	900: "hsl(220.909091deg 39.285714% 10.980392% / <alpha-value>)",
+	950: "hsl(224deg 71.428571% 4.117647% / <alpha-value>)",
+} as const;
+/** zinc */
+export const zinc = {
+	50: "hsl(0deg 0% 98.039216% / <alpha-value>)",
+	100: "hsl(240deg 4.761905% 95.882353% / <alpha-value>)",
+	200: "hsl(240deg 5.882353% 90% / <alpha-value>)",
+	300: "hsl(240deg 4.878049% 83.921569% / <alpha-value>)",
+	400: "hsl(240deg 5.027933% 64.901961% / <alpha-value>)",
+	500: "hsl(240deg 3.829787% 46.078431% / <alpha-value>)",
+	600: "hsl(240deg 5.202312% 33.921569% / <alpha-value>)",
+	700: "hsl(240deg 5.263158% 26.078431% / <alpha-value>)",
+	800: "hsl(240deg 3.703704% 15.882353% / <alpha-value>)",
+	900: "hsl(240deg 5.882353% 10% / <alpha-value>)",
+	950: "hsl(240deg 10% 3.921569% / <alpha-value>)",
+} as const;
+/** neutral */
+export const neutral = {
+	50: "hsl(0deg 0% 98.039216% / <alpha-value>)",
+	100: "hsl(0deg 0% 96.078431% / <alpha-value>)",
+	200: "hsl(0deg 0% 89.803922% / <alpha-value>)",
+	300: "hsl(0deg 0% 83.137255% / <alpha-value>)",
+	400: "hsl(0deg 0% 63.921569% / <alpha-value>)",
+	500: "hsl(0deg 0% 45.098039% / <alpha-value>)",
+	600: "hsl(0deg 0% 32.156863% / <alpha-value>)",
+	700: "hsl(0deg 0% 25.098039% / <alpha-value>)",
+	800: "hsl(0deg 0% 14.901961% / <alpha-value>)",
+	900: "hsl(0deg 0% 9.019608% / <alpha-value>)",
+	950: "hsl(0deg 0% 3.921569% / <alpha-value>)",
+} as const;
+/** stone */
+export const stone = {
+	50: "hsl(60deg 9.090909% 97.843137% / <alpha-value>)",
+	100: "hsl(60deg 4.761905% 95.882353% / <alpha-value>)",
+	200: "hsl(20deg 5.882353% 90% / <alpha-value>)",
+	300: "hsl(24deg 5.747126% 82.941176% / <alpha-value>)",
+	400: "hsl(24deg 5.434783% 63.921569% / <alpha-value>)",
+	500: "hsl(25deg 5.263158% 44.705882% / <alpha-value>)",
+	600: "hsl(33.333333deg 5.454545% 32.352941% / <alpha-value>)",
+	700: "hsl(30deg 6.25% 25.098039% / <alpha-value>)",
+	800: "hsl(12deg 6.493506% 15.098039% / <alpha-value>)",
+	900: "hsl(24deg 9.803922% 10% / <alpha-value>)",
+	950: "hsl(20deg 14.285714% 4.117647% / <alpha-value>)",
+} as const;
+/** red */
+export const red = {
+	50: "hsl(0deg 85.714286% 97.254902% / <alpha-value>)",
+	100: "hsl(0deg 93.333333% 94.117647% / <alpha-value>)",
+	200: "hsl(0deg 96.296296% 89.411765% / <alpha-value>)",
+	300: "hsl(0deg 93.548387% 81.764706% / <alpha-value>)",
+	400: "hsl(0deg 90.604027% 70.784314% / <alpha-value>)",
+	500: "hsl(0deg 84.236453% 60.196078% / <alpha-value>)",
+	600: "hsl(0deg 72.222222% 50.588235% / <alpha-value>)",
+	700: "hsl(0deg 73.70892% 41.764706% / <alpha-value>)",
+	800: "hsl(0deg 70% 35.294118% / <alpha-value>)",
+	900: "hsl(0deg 62.820513% 30.588235% / <alpha-value>)",
+	950: "hsl(0deg 74.683544% 15.490196% / <alpha-value>)",
+} as const;
+/** orange */
+export const orange = {
+	50: "hsl(33.333333deg 100% 96.470588% / <alpha-value>)",
+	100: "hsl(34.285714deg 100% 91.764706% / <alpha-value>)",
+	200: "hsl(32.142857deg 97.674419% 83.137255% / <alpha-value>)",
+	300: "hsl(30.656934deg 97.163121% 72.352941% / <alpha-value>)",
+	400: "hsl(27.015707deg 95.979899% 60.980392% / <alpha-value>)",
+	500: "hsl(24.581498deg 94.979079% 53.137255% / <alpha-value>)",
+	600: "hsl(20.540541deg 90.243902% 48.235294% / <alpha-value>)",
+	700: "hsl(17.472527deg 88.349515% 40.392157% / <alpha-value>)",
+	800: "hsl(15deg 79.069767% 33.72549% / <alpha-value>)",
+	900: "hsl(15.283019deg 74.647887% 27.843137% / <alpha-value>)",
+	950: "hsl(13deg 81.081081% 14.509804% / <alpha-value>)",
+} as const;
+/** amber */
+export const amber = {
+	50: "hsl(48deg 100% 96.078431% / <alpha-value>)",
+	100: "hsl(48deg 96.491228% 88.823529% / <alpha-value>)",
+	200: "hsl(48deg 96.638655% 76.666667% / <alpha-value>)",
+	300: "hsl(45.942857deg 96.685083% 64.509804% / <alpha-value>)",
+	400: "hsl(43.255814deg 96.412556% 56.27451% / <alpha-value>)",
+	500: "hsl(37.692308deg 92.125984% 50.196078% / <alpha-value>)",
+	600: "hsl(32.132701deg 94.618834% 43.72549% / <alpha-value>)",
+	700: "hsl(25.964912deg 90.47619% 37.058824% / <alpha-value>)",
+	800: "hsl(22.727273deg 82.5% 31.372549% / <alpha-value>)",
+	900: "hsl(21.714286deg 77.777778% 26.470588% / <alpha-value>)",
+	950: "hsl(20.909091deg 91.666667% 14.117647% / <alpha-value>)",
+} as const;
+/** yellow */
+export const yellow = {
+	50: "hsl(54.545455deg 91.666667% 95.294118% / <alpha-value>)",
+	100: "hsl(54.915254deg 96.721311% 88.039216% / <alpha-value>)",
+	200: "hsl(52.758621deg 98.305085% 76.862745% / <alpha-value>)",
+	300: "hsl(50.43956deg 97.849462% 63.529412% / <alpha-value>)",
+	400: "hsl(47.947598deg 95.8159% 53.137255% / <alpha-value>)",
+	500: "hsl(45.39823deg 93.38843% 47.45098% / <alpha-value>)",
+	600: "hsl(40.606061deg 96.116505% 40.392157% / <alpha-value>)",
+	700: "hsl(35.454545deg 91.666667% 32.941176% / <alpha-value>)",
+	800: "hsl(31.764706deg 80.952381% 28.823529% / <alpha-value>)",
+	900: "hsl(28.421053deg 72.519084% 25.686275% / <alpha-value>)",
+	950: "hsl(26deg 83.333333% 14.117647% / <alpha-value>)",
+} as const;
+/** lime */
+export const lime = {
+	50: "hsl(78.26087deg 92% 95.098039% / <alpha-value>)",
+	100: "hsl(79.591837deg 89.090909% 89.215686% / <alpha-value>)",
+	200: "hsl(80.869565deg 88.461538% 79.607843% / <alpha-value>)",
+	300: "hsl(81.971831deg 84.52381% 67.058824% / <alpha-value>)",
+	400: "hsl(82.711864deg 77.973568% 55.490196% / <alpha-value>)",
+	500: "hsl(83.736264deg 80.530973% 44.313725% / <alpha-value>)",
+	600: "hsl(84.8deg 85.227273% 34.509804% / <alpha-value>)",
+	700: "hsl(85.87156deg 78.417266% 27.254902% / <alpha-value>)",
+	800: "hsl(86.25deg 68.965517% 22.745098% / <alpha-value>)",
+	900: "hsl(87.619048deg 61.165049% 20.196078% / <alpha-value>)",
+	950: "hsl(89.268293deg 80.392157% 10% / <alpha-value>)",
+} as const;
+/** green */
+export const green = {
+	50: "hsl(138.461538deg 76.470588% 96.666667% / <alpha-value>)",
+	100: "hsl(140.625deg 84.210526% 92.54902% / <alpha-value>)",
+	200: "hsl(141deg 78.947368% 85.098039% / <alpha-value>)",
+	300: "hsl(141.714286deg 76.642336% 73.137255% / <alpha-value>)",
+	400: "hsl(141.891892deg 69.158879% 58.039216% / <alpha-value>)",
+	500: "hsl(142.08589deg 70.562771% 45.294118% / <alpha-value>)",
+	600: "hsl(142.12766deg 76.216216% 36.27451% / <alpha-value>)",
+	700: "hsl(142.429907deg 71.812081% 29.215686% / <alpha-value>)",
+	800: "hsl(142.78481deg 64.227642% 24.117647% / <alpha-value>)",
+	900: "hsl(143.809524deg 61.165049% 20.196078% / <alpha-value>)",
+	950: "hsl(144.878049deg 80.392157% 10% / <alpha-value>)",
+} as const;
+/** emerald */
+export const emerald = {
+	50: "hsl(151.764706deg 80.952381% 95.882353% / <alpha-value>)",
+	100: "hsl(149.268293deg 80.392157% 90% / <alpha-value>)",
+	200: "hsl(152.368421deg 76% 80.392157% / <alpha-value>)",
+	300: "hsl(156.198347deg 71.597633% 66.862745% / <alpha-value>)",
+	400: "hsl(158.113208deg 64.37247% 51.568627% / <alpha-value>)",
+	500: "hsl(160.118343deg 84.079602% 39.411765% / <alpha-value>)",
+	600: "hsl(161.37931deg 93.548387% 30.392157% / <alpha-value>)",
+	700: "hsl(162.931034deg 93.548387% 24.313725% / <alpha-value>)",
+	800: "hsl(163.146067deg 88.118812% 19.803922% / <alpha-value>)",
+	900: "hsl(164.166667deg 85.714286% 16.470588% / <alpha-value>)",
+	950: "hsl(165.714286deg 91.304348% 9.019608% / <alpha-value>)",
+} as const;
+/** teal */
+export const teal = {
+	50: "hsl(166.153846deg 76.470588% 96.666667% / <alpha-value>)",
+	100: "hsl(167.234043deg 85.454545% 89.215686% / <alpha-value>)",
+	200: "hsl(168.387097deg 83.783784% 78.235294% / <alpha-value>)",
+	300: "hsl(170.571429deg 76.923077% 64.313725% / <alpha-value>)",
+	400: "hsl(172.45509deg 66.007905% 50.392157% / <alpha-value>)",
+	500: "hsl(173.414634deg 80.392157% 40% / <alpha-value>)",
+	600: "hsl(174.666667deg 83.850932% 31.568627% / <alpha-value>)",
+	700: "hsl(175.339806deg 77.443609% 26.078431% / <alpha-value>)",
+	800: "hsl(176.103896deg 69.369369% 21.764706% / <alpha-value>)",
+	900: "hsl(175.932203deg 60.824742% 19.019608% / <alpha-value>)",
+	950: "hsl(178.604651deg 84.313725% 10% / <alpha-value>)",
+} as const;
+/** cyan */
+export const cyan = {
+	50: "hsl(183.157895deg 100% 96.27451% / <alpha-value>)",
+	100: "hsl(185.106383deg 95.918367% 90.392157% / <alpha-value>)",
+	200: "hsl(186.206897deg 93.548387% 81.764706% / <alpha-value>)",
+	300: "hsl(186.986301deg 92.405063% 69.019608% / <alpha-value>)",
+	400: "hsl(187.941176deg 85.714286% 53.333333% / <alpha-value>)",
+	500: "hsl(188.737864deg 94.495413% 42.745098% / <alpha-value>)",
+	600: "hsl(191.647059deg 91.397849% 36.470588% / <alpha-value>)",
+	700: "hsl(192.923077deg 82.278481% 30.980392% / <alpha-value>)",
+	800: "hsl(194.375deg 69.565217% 27.058824% / <alpha-value>)",
+	900: "hsl(196.363636deg 63.636364% 23.72549% / <alpha-value>)",
+	950: "hsl(197deg 78.947368% 14.901961% / <alpha-value>)",
+} as const;
+/** sky */
+export const sky = {
+	50: "hsl(204deg 100% 97.058824% / <alpha-value>)",
+	100: "hsl(204deg 93.75% 93.72549% / <alpha-value>)",
+	200: "hsl(200.597015deg 94.366197% 86.078431% / <alpha-value>)",
+	300: "hsl(199.370079deg 95.488722% 73.921569% / <alpha-value>)",
+	400: "hsl(198.4375deg 93.203883% 59.607843% / <alpha-value>)",
+	500: "hsl(198.630137deg 88.663968% 48.431373% / <alpha-value>)",
+	600: "hsl(200.406091deg 98.00995% 39.411765% / <alpha-value>)",
+	700: "hsl(201.265823deg 96.341463% 32.156863% / <alpha-value>)",
+	800: "hsl(200.952381deg 90% 27.45098% / <alpha-value>)",
+	900: "hsl(202.040816deg 80.327869% 23.921569% / <alpha-value>)",
+	950: "hsl(204deg 80.246914% 15.882353% / <alpha-value>)",
+} as const;
+/** blue */
+export const blue = {
+	50: "hsl(213.75deg 100% 96.862745% / <alpha-value>)",
+	100: "hsl(214.285714deg 94.594595% 92.745098% / <alpha-value>)",
+	200: "hsl(213.333333deg 96.923077% 87.254902% / <alpha-value>)",
+	300: "hsl(211.698113deg 96.363636% 78.431373% / <alpha-value>)",
+	400: "hsl(213.116883deg 93.902439% 67.843137% / <alpha-value>)",
+	500: "hsl(217.219251deg 91.219512% 59.803922% / <alpha-value>)",
+	600: "hsl(221.212121deg 83.193277% 53.333333% / <alpha-value>)",
+	700: "hsl(224.278075deg 76.326531% 48.039216% / <alpha-value>)",
+	800: "hsl(225.931034deg 70.731707% 40.196078% / <alpha-value>)",
+	900: "hsl(224.444444deg 64.285714% 32.941176% / <alpha-value>)",
+	950: "hsl(226.229508deg 57.009346% 20.980392% / <alpha-value>)",
+} as const;
+/** indigo */
+export const indigo = {
+	50: "hsl(225.882353deg 100% 96.666667% / <alpha-value>)",
+	100: "hsl(226.451613deg 100% 93.921569% / <alpha-value>)",
+	200: "hsl(228deg 96.491228% 88.823529% / <alpha-value>)",
+	300: "hsl(229.655172deg 93.548387% 81.764706% / <alpha-value>)",
+	400: "hsl(234.453782deg 89.473684% 73.921569% / <alpha-value>)",
+	500: "hsl(238.732394deg 83.529412% 66.666667% / <alpha-value>)",
+	600: "hsl(243.396226deg 75.35545% 58.627451% / <alpha-value>)",
+	700: "hsl(244.520548deg 57.936508% 50.588235% / <alpha-value>)",
+	800: "hsl(243.652174deg 54.50237% 41.372549% / <alpha-value>)",
+	900: "hsl(242.168675deg 47.428571% 34.313725% / <alpha-value>)",
+	950: "hsl(243.75deg 47.058824% 20% / <alpha-value>)",
+} as const;
+/** violet */
+export const violet = {
+	50: "hsl(250deg 100% 97.647059% / <alpha-value>)",
+	100: "hsl(251.428571deg 91.304348% 95.490196% / <alpha-value>)",
+	200: "hsl(250.5deg 95.238095% 91.764706% / <alpha-value>)",
+	300: "hsl(252.5deg 94.736842% 85.098039% / <alpha-value>)",
+	400: "hsl(255.135135deg 91.735537% 76.27451% / <alpha-value>)",
+	500: "hsl(258.311688deg 89.534884% 66.27451% / <alpha-value>)",
+	600: "hsl(262.122905deg 83.255814% 57.843137% / <alpha-value>)",
+	700: "hsl(263.389831deg 69.960474% 50.392157% / <alpha-value>)",
+	800: "hsl(263.355705deg 69.302326% 42.156863% / <alpha-value>)",
+	900: "hsl(263.5deg 67.41573% 34.901961% / <alpha-value>)",
+	950: "hsl(261.176471deg 72.649573% 22.941176% / <alpha-value>)",
+} as const;
+/** purple */
+export const purple = {
+	50: "hsl(270deg 100% 98.039216% / <alpha-value>)",
+	100: "hsl(268.695652deg 100% 95.490196% / <alpha-value>)",
+	200: "hsl(268.571429deg 100% 91.764706% / <alpha-value>)",
+	300: "hsl(269.189189deg 97.368421% 85.098039% / <alpha-value>)",
+	400: "hsl(270deg 95.238095% 75.294118% / <alpha-value>)",
+	500: "hsl(270.740741deg 91.011236% 65.098039% / <alpha-value>)",
+	600: "hsl(271.47541deg 81.333333% 55.882353% / <alpha-value>)",
+	700: "hsl(272.093023deg 71.666667% 47.058824% / <alpha-value>)",
+	800: "hsl(272.888889deg 67.164179% 39.411765% / <alpha-value>)",
+	900: "hsl(273.64486deg 65.644172% 31.960784% / <alpha-value>)",
+	950: "hsl(273.548387deg 86.915888% 20.980392% / <alpha-value>)",
+} as const;
+/** fuchsia */
+export const fuchsia = {
+	50: "hsl(289.090909deg 100% 97.843137% / <alpha-value>)",
+	100: "hsl(286.956522deg 100% 95.490196% / <alpha-value>)",
+	200: "hsl(288.26087deg 95.833333% 90.588235% / <alpha-value>)",
+	300: "hsl(291.111111deg 93.103448% 82.941176% / <alpha-value>)",
+	400: "hsl(292.03125deg 91.428571% 72.54902% / <alpha-value>)",
+	500: "hsl(292.189349deg 84.079602% 60.588235% / <alpha-value>)",
+	600: "hsl(293.410405deg 69.477912% 48.823529% / <alpha-value>)",
+	700: "hsl(294.693878deg 72.413793% 39.803922% / <alpha-value>)",
+	800: "hsl(295.423729deg 70.238095% 32.941176% / <alpha-value>)",
+	900: "hsl(296.703297deg 63.636364% 28.039216% / <alpha-value>)",
+	950: "hsl(296.756757deg 90.243902% 16.078431% / <alpha-value>)",
+} as const;
+/** pink */
+export const pink = {
+	50: "hsl(327.272727deg 73.333333% 97.058824% / <alpha-value>)",
+	100: "hsl(325.714286deg 77.777778% 94.705882% / <alpha-value>)",
+	200: "hsl(325.909091deg 84.615385% 89.803922% / <alpha-value>)",
+	300: "hsl(327.407407deg 87.096774% 81.764706% / <alpha-value>)",
+	400: "hsl(328.615385deg 85.526316% 70.196078% / <alpha-value>)",
+	500: "hsl(330.365854deg 81.188119% 60.392157% / <alpha-value>)",
+	600: "hsl(333.333333deg 71.428571% 50.588235% / <alpha-value>)",
+	700: "hsl(335.060241deg 77.570093% 41.960784% / <alpha-value>)",
+	800: "hsl(335.820896deg 74.444444% 35.294118% / <alpha-value>)",
+	900: "hsl(335.88785deg 69.032258% 30.392157% / <alpha-value>)",
+	950: "hsl(336.164384deg 83.908046% 17.058824% / <alpha-value>)",
+} as const;
+/** rose */
+export const rose = {
+	50: "hsl(355.714286deg 100% 97.254902% / <alpha-value>)",
+	100: "hsl(355.555556deg 100% 94.705882% / <alpha-value>)",
+	200: "hsl(352.653061deg 96.078431% 90% / <alpha-value>)",
+	300: "hsl(352.58427deg 95.698925% 81.764706% / <alpha-value>)",
+	400: "hsl(351.304348deg 94.520548% 71.372549% / <alpha-value>)",
+	500: "hsl(349.723757deg 89.162562% 60.196078% / <alpha-value>)",
+	600: "hsl(346.836735deg 77.165354% 49.803922% / <alpha-value>)",
+	700: "hsl(345.348837deg 82.692308% 40.784314% / <alpha-value>)",
+	800: "hsl(343.404255deg 79.661017% 34.705882% / <alpha-value>)",
+	900: "hsl(341.538462deg 75.483871% 30.392157% / <alpha-value>)",
+	950: "hsl(343.098592deg 87.654321% 15.882353% / <alpha-value>)",
+} as const;
